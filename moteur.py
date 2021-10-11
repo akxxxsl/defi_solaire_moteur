265311f012dd169b6246068b977590ff1ad64b0f
Elieroc <34548349+Elieroc@users.noreply.github.com>
2021-10-11 11:13:42 +0200

    Moteur accélération

---
+++ moteur.py
@@ -0,0 +1,33 @@
+# Période : 20 ms
+# Fréquence : 50 Hz
+# Rapport cyclique : 1/10
+# Pin 5 : D1
+
+from machine import Pin,PWM 
+import time
+
+freq = 50
+duty = 80
+
+moteur=PWM(Pin(5))
+
+def start(freq, duty):
+  # Démarrage à vitesse initiale
+  moteur.freq(freq)
+  moteur.duty(duty)
+  
+def stop():
+  # Arrêt des moteurs
+  moteur.freq(0)
+  moteur.duty(0)
+
+def main():
+  # Démarrage des moteurs
+  start(freq, duty)
+  # On fait tourner les moteurs pendant 2 sec
+  #time.sleep(2)
+  # On arrête tout
+  #stop()
+
+if __name__ == "__main__":
+  main()
